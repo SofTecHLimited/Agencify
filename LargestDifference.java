75058a2c978b13c48fd92c88c56251beddb7b2e4
SofTecHLimited <124133866+SofTecHLimited@users.noreply.github.com>
2023-06-22 10:30:46 +0300

    Add files via upload

---
+++ LargestDifference.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class LargestDifference {
+    public static int findLargestDifference(List<Integer> numbers) {
+        if (numbers.size() < 2) {
+            return 0;
+        }
+
+        int minNumber = numbers.get(0);
+        int maxDifference = 0;
+
+        for (int i = 1; i < numbers.size(); i++) {
+            int currentNumber = numbers.get(i);
+            maxDifference = Math.max(maxDifference, currentNumber - minNumber);
+            minNumber = Math.min(minNumber, currentNumber);
+        }
+
+        return maxDifference;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 9, 3, 7);
+        int largestDifference = findLargestDifference(numbers);
+        System.out.println("The largest difference is: " + largestDifference);
+    }
+}
